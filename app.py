b0bd11fc99e08bc4d5f3e8d08cf2b175bda2a34d
joe <joe@mojosuper.market>
2024-04-11 15:38:58 -0400

    lots

--- app.py
+++ app.py
@@ -11,11 +11,16 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Ensure the upload folder exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    # Get list of image filenames in the upload directory
+    image_files = [file for file in os.listdir(UPLOAD_FOLDER) if file.endswith('.gif')]
+    # Sort the files by modified time
+    image_files.sort(key=lambda x: os.path.getmtime(os.path.join(UPLOAD_FOLDER, x)), reverse=True)
+    # Select the last four "shaken" images
+    last_four_images = image_files[:4]
+    # Render the template with the images
+    return render_template('index.html', images=last_four_images)
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
@@ -29,7 +34,6 @@ def upload_image():
         file.save(filepath)
         gif_path = create_shaking_gif(filepath)
         return send_from_directory(os.path.dirname(gif_path), os.path.basename(gif_path))
-
 
 def create_shaking_gif(filepath):
     img = Image.open(filepath)
@@ -47,7 +51,6 @@ def create_shaking_gif(filepath):
     frames[0].save(gif_path, save_all=True, append_images=frames[1:], optimize=False, duration=100, loop=0)
 
     return gif_path
-
 
 if __name__ == '__main__':
     app.run(debug=True)
